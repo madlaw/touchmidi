c4e931fb85440a2cb416ee9cb1983672f860e19e
Michael Lawson <michael@mikelawson.ca>
2013-07-28 09:53:42 -0400

    Added some extra scales.  All modes of the major scale, plus phrygian dominant.

--- notes.py
+++ notes.py
@@ -6,17 +6,28 @@ def scale(note, key):
   """
   TONE = 2
   SEMITONE = 1
-  
+
+	"""
+	ionian - major scale
+	aeolian - minor scale
+	phrygian_dominan - ``jewish scale''
+	"""
   keys = {
-      "major": [TONE, TONE, SEMITONE, TONE, TONE, TONE], 
-      "natural_minor": [TONE, SEMITONE, TONE, TONE, SEMITONE, TONE], 
-      "minor_pent": [3, TONE, TONE, 3], 
-      "major_pent": [TONE, TONE, 3, TONE]
+      "ionian": [TONE, TONE, SEMITONE, TONE, TONE, TONE],
+			"dorian": [TONE, SEMITONE, TONE, TONE, TONE, SEMITONE],
+			"phrygian": [SEMITONE, TONE, TONE, TONE, SEMITONE, TONE],
+			"phrygian_dominant": [SEMITONE, 3, SEMITONE, TONE, SEMITONE, TONE],
+			"lydian": [TONE, TONE, TONE, SEMITONE, TONE, TONE],
+			"mixolydian": [TONE, TONE, SEMITONE, TONE, TONE, SEMITONE],
+			"aeolian": [TONE, SEMITONE, TONE, TONE, SEMITONE, TONE],
+			"locrian": [SEMITONE, TONE, TONE, SEMITONE, TONE, TONE],
+      "minor_pent": [3, TONE, TONE, 3, TONE],
+      "major_pent": [TONE, TONE, 3, TONE, TONE]
     }
 
   notes = [n.split(" ") for n in open("notes", "r").read().strip().split("\n")]
   notes = {a[0]:int(a[-1]) for a in notes}
-  
+
   if note in notes and key in keys:
     values = []
     values.append(notes[note])
